5cffc9b8f9b920cb0f4e87e0940c0a36e5fb6a99
ilyapashayan <ilya.w.a.j@gmail.com>
2023-07-15 02:39:55 +0400

    auth modal window development

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,10 +1,49 @@
-import type { FC } from 'react'
+import { FC, useState } from 'react'
 
 import { HeartIcon, ListIcon, LogoIcon, SearchIcon, ShopCartIcon, UserIcon } from 'assets'
 
 import styles from './Header.module.scss'
+import { Button, Checkbox, Modal } from 'components/atom'
+import InputAuth from 'components/atom/Input/Auth'
+import { TModalState } from './type'
 
 const Header: FC = () => {
+  const [modalState, setModalState] = useState<TModalState>({
+    title: 'Вхід',
+    showLogin: true,
+    showLoginConfirm: false,
+    showRegister: false,
+    showRegisterConfirm: false,
+  })
+
+  const handleLoginClick = () => {
+    setModalState({
+      title: '',
+      showLogin: false,
+      showLoginConfirm: true,
+    })
+  }
+  const handleRegisterClick = () => {
+    setModalState({
+      title: 'Реєстрація',
+      showLoginConfirm: false,
+      showRegister: true,
+    })
+  }
+  const handleRegisterConfirmClick = () => {
+    setModalState({
+      showRegister: false,
+      showRegisterConfirm: true,
+    })
+  }
+
+  const showLogin = () => {
+    setModalState({
+      title: 'Вхід',
+      showLogin: true,
+    })
+  }
+
   return (
     <header className={styles.wrapper}>
       <LogoIcon className={styles.wrapper__logo} />
@@ -42,6 +81,59 @@ const Header: FC = () => {
           <p>Увійти</p>
         </div>
       </div>
+      <Modal title={modalState.title}>
+        {modalState.showLogin && (
+          <div className={styles.modal__form}>
+            <InputAuth lable='Телефон' type='tel' placeholder='+38 (0__) ___-__-__' />
+            <Checkbox title='Запам’ятати мене' />
+            <Button onClick={handleLoginClick}>Увійти</Button>
+            <button onClick={handleRegisterClick} className={styles.modal__form__link}>
+              Зареєструватися
+            </button>
+          </div>
+        )}
+
+        {modalState.showLoginConfirm && (
+          <div className={styles.modal__login__confirm}>
+            <p>
+              Для підтвердження входу в обліковий запис, відкрийте <b>Telegram</b> та натисніть кнопку з цифрою:
+            </p>
+            <span>67</span>
+            <div className={styles.modal__login__confirm__buttons}>
+              <Button className={styles.modal__login__confirm__buttons__btn1}>Відновлення доступу</Button>
+              <Button className={styles.modal__login__confirm__buttons__btn2}>Відмінити вхід</Button>
+            </div>
+          </div>
+        )}
+
+        {modalState.showRegister && (
+          <div className={styles.modal__form}>
+            <InputAuth lable='Ім’я' type='text' />
+            <InputAuth lable='Телефон' type='tel' placeholder='+38 (0__) ___-__-__' />
+            <p className={styles.modal__form__frame}>
+              Реєструючись, ви погоджуєтеся з умовами <b>положення про обробку і захист персональних даних</b> та{' '}
+              <b>угодою користувача</b>
+            </p>
+            <Button onClick={handleRegisterConfirmClick}>Зареєструватися</Button>
+            <button onClick={showLogin} className={styles.wrapper__link}>
+              Я вже зареєстрований
+            </button>
+          </div>
+        )}
+
+        {modalState.showRegisterConfirm && (
+          <div className={styles.modal__login__confirm}>
+            <p>
+              Для підтвердження входу в обліковий запис, відкрийте <b>Telegram</b> та натисніть кнопку з цифрою:
+            </p>
+            <span>49</span>
+            <div className={styles.modal__login__confirm__buttons}>
+              <Button className={styles.modal__login__confirm__buttons__btn1}>Відкрити Telegram</Button>
+              <Button className={styles.modal__login__confirm__buttons__btn2}>Відмінити вхід</Button>
+            </div>
+          </div>
+        )}
+      </Modal>
     </header>
   )
 }
